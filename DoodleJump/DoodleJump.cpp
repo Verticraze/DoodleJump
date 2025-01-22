612c65ec68a15301ee603e2d4d89ff21a669eac2
Verticraze <verticraze@outlook.com>
2025-01-22 17:41:05 +0530

    Added the core game loop

--- DoodleJump/DoodleJump.cpp
+++ DoodleJump/DoodleJump.cpp
@@ -6,7 +6,8 @@ struct point { int x, y;};
 int main()
 {
 	srand(time(0));
-	sf::RenderWindow app(sf::VideoMode(640, 360), "Doodle Jump");
+	sf::RenderWindow app(sf::VideoMode(640, 800), "Doodle Jump");
+	app.setFramerateLimit(60);
 
 	sf::Texture t1, t2, t3;
 
@@ -15,7 +16,15 @@ int main()
 	sf::Sprite spriteBackground(t3);
 
 	point plat[20];
-	int x, y;
+	int x =100, y=100,h=200;
+	float dx=0.0, dy=0.0;
+
+	for (int i=0;i<10;i++)
+	{
+		plat[i].x = rand() % 640;
+		plat[i].y = rand() % 800;
+	}
+
 	while (app.isOpen())
 	{
 		sf::Event e;
@@ -29,6 +38,31 @@ int main()
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { x += 3; }
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {x-=3 ; }
+
+		dy += 0.2f;
+		y += dy;
+		if (y>500) {dy=-10;}
+
+		if (y > h) 
+		{
+			for (int i=0;i<10;i++)
+			{
+				y = h;
+				plat[i].y -= dy;
+				if (plat[i].y > 800) 
+				{ 
+					plat[i].y = 0; 
+					plat[i].x = rand() % 640;
+				};
+			}
+		}
+
+		for (int i = 0; i < 10; i++)
+		{
+			if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x < 68) && (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && dy > 0) { dy = 0; }
+		}
+
+		spritePlayer.setPosition(x, y);
 
 		app.draw(spriteBackground);
 		app.draw(spritePlayer);
