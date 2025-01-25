389dc871be1ee8355597dd6b0e408d8420302eab
Verticraze <verticraze@outlook.com>
2025-01-25 21:47:22 +0530

    Updated the game logic

--- DoodleJump/DoodleJump.cpp
+++ DoodleJump/DoodleJump.cpp
@@ -6,10 +6,16 @@ struct point { int x, y;};
 int main()
 {
 	srand(time(0));
-	sf::RenderWindow app(sf::VideoMode(640, 800), "Doodle Jump");
+
+	sf::RenderWindow app(sf::VideoMode(400, 533), "Doodle Jump");
+	
 	app.setFramerateLimit(60);
 
 	sf::Texture t1, t2, t3;
+
+	t1.loadFromFile("doodle.png");
+	t2.loadFromFile("platform.png");
+	t3.loadFromFile("background.png");
 
 	sf::Sprite spritePlayer(t1);
 	sf::Sprite spritePlatform(t2);
@@ -17,12 +23,12 @@ int main()
 
 	point plat[20];
 	int x =100, y=100,h=200;
-	float dx=0.0, dy=0.0;
+	float dx=0, dy=0;
 
 	for (int i=0;i<10;i++)
 	{
-		plat[i].x = rand() % 640;
-		plat[i].y = rand() % 800;
+		plat[i].x = rand() % 400;
+		plat[i].y = rand() % 533;
 	}
 
 	while (app.isOpen())
@@ -43,24 +49,24 @@ int main()
 		y += dy;
 		if (y>500) {dy=-10;}
 
-		if (y > h) 
+		if (y < h) 
 		{
 			for (int i=0;i<10;i++)
 			{
 				y = h;
-				plat[i].y -= dy;
-				if (plat[i].y > 800) 
+				plat[i].y = plat[i].y - dy;
+				if (plat[i].y > 533) 
 				{ 
 					plat[i].y = 0; 
-					plat[i].x = rand() % 640;
+					plat[i].x = rand() % 400;
 				};
 			}
 		}
 
 		for (int i = 0; i < 10; i++)
 		{
-			if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x < 68) && (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && dy > 0) { dy = 0; }
-		}
+			if ((x + 50 > plat[i].x) && (x + 20 < plat[i].x + 68) && (y + 70 > plat[i].y) && (y + 70 < plat[i].y + 14) && (dy > 0)) { dy = -10; }
+		}	
 
 		spritePlayer.setPosition(x, y);
 
@@ -68,7 +74,8 @@ int main()
 		app.draw(spritePlayer);
 		for (int i=0;i<10;i++)
 		{
-			
+			spritePlatform.setPosition(plat[i].x,plat[i].y);
+			app.draw(spritePlatform);
 		}
 		app.display();
 	}
